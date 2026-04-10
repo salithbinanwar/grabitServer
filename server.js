f098b4032cdfc9e22c95cd8b6ba9c3ec271b9302
salithbinanwar <salithanwar69@gmail.com>
2026-04-10 15:37:17 +0800

    new env added

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true, // Important for cookies
+    origin: ['https://your-netlify-app.netlify.app', 'http://localhost:5173'],
+    credentials: true,
   }),
 )
 app.use(express.json())
@@ -28,7 +28,7 @@ app.use('/api/auth', authRoutes)
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('Grabit API is running 🚀')
+  res.send('Grabit API is running')
 })
 
 // Connect to MongoDB
